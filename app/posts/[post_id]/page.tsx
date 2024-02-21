540b767bdc132f05ec09db8dbdb3352804cd3db9
jed peek <jedpeek@MacBook-Pro-3.local>
2024-02-21 12:49:24 -0800

    fixed posts

--- app/posts/[post_id]/page.tsx
+++ app/posts/[post_id]/page.tsx
@@ -1,28 +1,28 @@
-// "use client";
+"use client";
 // import { useParams } from "next/navigation";
 // import { BLOG_DATA } from "/BLOG_DATA";
 
-// const Post = () => {
-//   const data = BLOG_DATA;
-//   const { post_id } = useParams();
-//   const post = data.find((post: any) => post.post_id === post_id);
-//   return (
-//     <>
-//       {post ? (
-//         <div>
-//           <h1>AUTHOR: {post.author}</h1>
-//           <div>
-//             <h1>TITLE: {post.title}</h1>
-//           </div>
-//           <div>
-//             <p>{post.content}</p>
-//           </div>
-//         </div>
-//       ) : (
-//         <h1>CAN'T FIND POST</h1>
-//       )}
-//     </>
-//   );
-// };
+const Post = () => {
+  // const data = BLOG_DATA;
+  // const { post_id } = useParams();
+  // const post = data.find((post: any) => post.post_id === post_id);
+  return (
+    <>
+      {/* {post ? (
+        <div>
+          <h1>AUTHOR: {post.author}</h1>
+          <div>
+            <h1>TITLE: {post.title}</h1>
+          </div>
+          <div>
+            <p>{post.content}</p>
+          </div>
+        </div>
+      ) : (
+        <h1>CAN'T FIND POST</h1>
+      )} */}
+    </>
+  );
+};
 
-// export default Post;
+export default Post;
